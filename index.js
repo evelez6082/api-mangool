63487b1e2add5b87867459407699ec7e096537eb
Josué Vélez <josue.vlez@gmail.com>
2019-01-31 05:06:34 -0500

    asd

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
-app.use(cors({origin: 'http://35.235.69.15:4200'}));
+app.use(cors({origin: 'http://revenweb.com'}));
 
 
 //rutas del servidor
